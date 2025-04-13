66d74cf998546774af86c6e143b601f5f824bd19
AdityaAksar <adityaaksar40@gmail.com>
2025-04-14 01:47:26 +0800

    menambahkan like di index

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
             header("Location: login.php");
             exit();
         }
-        
 
         if(isset($_POST["kirim_post"])){
             $category = $_POST['category'];
@@ -89,12 +88,11 @@
                 $stmt->close();
             }
         }
-
-        $post_query = "
-            SELECT post.id_post, post.content, post.tanggal_posting, user.name
-            FROM post
-            JOIN user ON post.id_user = user.id_user
-            ORDER BY post.tanggal_posting DESC";
+        $post_query = "SELECT p.*, u.name, 
+                    (SELECT COUNT(*) FROM likes l WHERE l.id_post = p.id_post) AS like_count
+                    FROM post p
+                    JOIN user u ON p.id_user = u.id_user
+                    ORDER BY p.tanggal_posting DESC";
         $result_post = mysqli_query($db, $post_query);
 
         function getCommentsForPost($db, $post_id) {
@@ -110,7 +108,7 @@
             return $stmt->get_result();
         }
     ?>
-    <!-- NAVBAR -->
+    <!-- Navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 mb-5 bg-white rounded">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
@@ -193,17 +191,35 @@
     <div class="container">
         <div class="row justify-content-center">
             <?php 
-            while ($row = mysqli_fetch_array($result_post)) {
+            if (!$result_post) {
+                die("Query error: " . mysqli_error($db));
+            }
+
+            while ($row = mysqli_fetch_assoc($result_post)) {
                 $post_id = $row['id_post'];
+                $user_id = $_SESSION['id_user'];
+            
+                // Cek apakah user sudah like post ini
+                $like_check_query = "SELECT id_like FROM likes WHERE id_post = $post_id AND id_user = $user_id";
+                $like_check_result = mysqli_query($db, $like_check_query);
+                $is_liked = mysqli_num_rows($like_check_result) > 0;
+            
+                // Pilih warna tombol sesuai status like
+                $like_btn_class = $is_liked ? 'btn-danger' : 'btn-secondary';
+            
                 echo "<div class='col-12 mb-4'>
                     <div class='card mx-auto' style='max-width: 600px;'>
                         <div class='card-body'>
                             <h5 class='card-title mb-3'>".htmlspecialchars($row["name"])."</h5>
                             <h6 class='card-subtitle mb-2 text-body-secondary'>".htmlspecialchars($row["tanggal_posting"])."</h6>
-                            <p class='card-text'>".htmlspecialchars($row["content"])."</p>
-                            <a href='#' class='btn btn-secondary'>
-                                <img src='./node_modules/bootstrap-icons/icons/hand-thumbs-up.svg'> 0 Like
-                            </a>
+                            <p class='card-text'>".nl2br(htmlspecialchars($row["content"]))."</p>
+                            <form action='like.php' method='POST' class='d-inline'>
+                                <input type='hidden' name='post_id' value='".$post_id."'>
+                                <button type='submit' class='btn $like_btn_class'>
+                                    <img src='./node_modules/bootstrap-icons/icons/hand-thumbs-up.svg'> 
+                                    ".$row['like_count']." Like
+                                </button>
+                            </form>
                             <a href='#' class='btn btn-secondary' data-bs-toggle='modal' data-bs-target='#commentModal' 
                                 onclick='loadComments($post_id)'>
                                 <img src='./node_modules/bootstrap-icons/icons/chat.svg'> Comment
@@ -212,6 +228,7 @@
                     </div>
                 </div>";
             }
+            
             ?>
         </div>
     </div>
